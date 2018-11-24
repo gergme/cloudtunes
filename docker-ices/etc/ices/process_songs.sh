ed3b807cc64810223699494a484251afcf9b7e40
Greg Holthaus <gerg@gerg.me>
2018-11-23 21:35:40 -0800

    major updates

--- docker-ices/etc/ices/process_songs.sh
+++ docker-ices/etc/ices/process_songs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 lltag -F "/%i/%i/%a - %t" -G -R /data/default/* --sep _ --spaces -g "CloudTunes Rocks!" --yes
+normalize-ogg --bitrate 128 --tmpdir /tmp --force-encode --ogg /data/default/*.ogg
