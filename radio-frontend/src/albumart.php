e994c1e12c60b3b3451c41ec136eeb217b50cba8
Greg Holthaus <linuxguy83@gmail.com>
2019-05-01 16:30:04 -0500

    update logic to album art grabber

--- radio-frontend/src/albumart.php
+++ radio-frontend/src/albumart.php
@@ -1,8 +1,11 @@
 <?php
-require_once "lastfm.php";
 $m = new Memcached();
 $m->addServer('192.168.99.100', 31211) or die ("Cannot connect to memcached!");
-$cacheArt = $m->get('artist');
-$cacheTitle = $m->get('title');
-echo LastFMArtwork::getArtwork($cacheArt, $cacheTitle, true, "mega");
+$cacheChanged = $m->get('changed');
+if ( $cacheChanged != false OR $cacheChanged == null) {
+    require_once "lastfm.php";
+    $cacheArt = $m->get('artist');
+    $cacheTitle = $m->get('title');
+    echo LastFMArtwork::getArtwork($cacheArt, $cacheTitle, true, "mega");
+}
 ?>
--- radio-frontend/src/streaminfo.php
+++ radio-frontend/src/streaminfo.php
@@ -13,7 +13,8 @@ $cacheTitle = $m->get('title');
 $string = file_get_contents("http://192.168.99.100:30420/status-json.xsl");
 $json_a = json_decode($string, true);
 
-if ( $title != $cacheTitle ) {
+if ( $title != $cacheTitle OR $cacheTitle == null) {
+	$m->set('changed', true);
 	$m->set('artist', $art);
 	$m->set('title', $title);
 	//$m->set('album', $album);
@@ -22,6 +23,8 @@ if ( $title != $cacheTitle ) {
 	//$cacheAlbum = $m->get('album');
 	$fetchArt = shell_exec('/var/www/html/node_modules/album-art/cli.js "' . $cacheArt . '" "' . $cacheTitle . '"');
 	file_put_contents("img/currentAlbum.png", fopen($fetchArt, 'r+'));
+} else {
+	$m->set('changed', false);
 }
 
 // Output
