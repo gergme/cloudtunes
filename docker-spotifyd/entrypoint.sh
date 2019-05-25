754297b1c0bbebafbf3b00cfa6b373a71b2350f0
Greg Holthaus <linuxguy83@gmail.com>
2019-05-24 22:57:16 -0500

    add Spotify base

---
+++ docker-spotifyd/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z $USERNAME && -z $PASSWORD ]]
+then
+   echo You must provide env var USERNAME, PASSWORD
+   exit 0
+else
+   echo Your USERNAME is ${USERNAME} and your password is ${PASSWORD}
+fi
+
+DEVICE=${DEVICE:-$(echo $USERNAME | cut -d @ -f 1 | sed 's/\./-/' | sed 's/_//')}
+
+# Launch spotifyd in background
+spotifyd -u $USERNAME -p $PASSWORD --device_name $DEVICE
+
+exec spotifyd-http -u $USERNAME -p $PASSWORD -n `hostname`
+
